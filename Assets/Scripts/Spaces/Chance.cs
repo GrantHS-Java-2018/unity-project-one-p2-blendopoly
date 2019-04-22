234bd14b6bad3b228a501f5c247df50d77d6d8cc
Link <sandlincoln@gmail.com>
2019-04-22 03:50:05 -0700

    I'm completely changing how CC and Chance work. Fixed the previous bug.
    I need to either fix SpriteRenderer or make the cards into GameObjects. This is unfinished, but I understand what must be done.

--- Assets/Scripts/Spaces/Chance.cs
+++ Assets/Scripts/Spaces/Chance.cs
@@ -7,60 +7,14 @@ using Random = System.Random;
 
 public class Chance : GameTile
 {
-
-    public SpriteRenderer sr;
-
-    public Sprite[] spriteList = new Sprite[16];
-
-    public Sprite empty;
-    
-    private ArrayList alreadyChosen = new ArrayList();
-    
-    private Random chooser = new Random();
-
     void Start()
     {
-        sr.sprite = empty;
+        pos = GetComponent<Transform>().position;
     }
     
     public override void onLand(Player player)
     {
-        int randomInt;
-
-        if (alreadyChosen.Count >= 16)
-        {
-            alreadyChosen.Clear();
-        }
-
-        do
-        {
-            randomInt = chooser.Next(0, 16);
-        } while (inside(randomInt));
-
-        alreadyChosen.Add(randomInt);
-
-        sr.sprite = spriteList[randomInt];
-        
-        Thread.Sleep(1000);
-        
         player.readyForAction();
-        sr.sprite = null;
     }
 
-    private bool inside(int searchFor)
-    {
-        if (alreadyChosen.Count <= 0)
-        {
-            return false;
-        }
-        
-        foreach(int x1 in alreadyChosen){
-            if (x1 == searchFor)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }

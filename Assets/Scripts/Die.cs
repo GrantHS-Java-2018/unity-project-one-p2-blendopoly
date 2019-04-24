c87722622e0c15bf33e248913173e22a034bce9c
DExter <dexterwest317@gmail.com>
2019-04-24 12:10:35 -0700

    Added Dice

---
+++ Assets/Scripts/Die.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Spaces {
+ 
+    public class Die : MonoBehaviour
+    {
+        private Transform pos;
+
+        public void roll(int value)
+        {
+            
+        }
+        
+        
+    }
+}

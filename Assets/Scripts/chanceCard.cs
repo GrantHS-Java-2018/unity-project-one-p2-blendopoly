abe8fffaecfc451eca8117f6fca82f7cc684f313
Link <sandlincoln@gmail.com>
2019-04-25 10:51:46 -0700

    add the Random Chooser
    I still don't know why it won't derender properly.

--- Assets/Scripts/chanceCard.cs
+++ Assets/Scripts/chanceCard.cs
@@ -9,21 +9,44 @@ public class chanceCard : MonoBehaviour
     
     public Card[] cardList = new Card[16];
     
+    private List<int> notChosen = new List<int>();
+    
     void Start()
     {
         gameObject.GetComponent<Image>().sprite = null;
         gameObject.SetActive(false);
+        Vector3 scale;
+        scale.x = 2;
+        scale.y = 2;
+        scale.z = 1;
+        gameObject.GetComponent<RectTransform>().localScale = scale;
     }
 
     public void renderOn()
     {
-        gameObject.GetComponent<Image>().sprite = cardList[1].renderedSprite;
+        if (notChosen.Count <= 0)
+        {
+            reset();
+        }
+
+        int index = Random.Range(0, notChosen.Count);
+        gameObject.GetComponent<Image>().sprite = cardList[index].renderedSprite;
         gameObject.SetActive(true);
+        //Thread.Sleep(1000);
+        //renderOff();
     }
     
     public void renderOff()
     {
         gameObject.GetComponent<Image>().sprite = null;
         gameObject.SetActive(false);
+    }
+    
+    private void reset()
+    {
+        for (int x1 = 0; x1 < 16; ++x1)
+        {
+            notChosen.Add(x1);
+        }
     }
 }

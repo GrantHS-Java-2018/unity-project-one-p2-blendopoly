99e2f83e8d71c976e439f098a6c7f454ab257205
DExter <dexterwest317@gmail.com>
2019-04-26 10:39:44 -0700

    Die sort of works

--- Assets/Scripts/Die.cs
+++ Assets/Scripts/Die.cs
@@ -4,13 +4,19 @@ namespace Spaces {
  
     public class Die : MonoBehaviour
     {
+        public int faceShowing;
         private Transform pos;
+        public Vector3[] angles = new Vector3[6];
 
         public void roll(int value)
         {
             
         }
-        
+
+        private void setRotation()
+        {
+            
+        }
         
     }
 }

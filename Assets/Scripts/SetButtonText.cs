aab5c386dfeab8d44d9ba8cb0d47af7ef11e00f2
lolshoc <lolshoc@gmail.com>
2019-05-31 10:29:04 -0700

    Added the name change on the buttons.

---
+++ Assets/Scripts/SetButtonText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetButtonText : MonoBehaviour
+{
+
+    [SerializeField] private int player;
+    [SerializeField] private Text text;
+    [SerializeField] private PlayerHandler handler;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (handler.players.Length > player)
+        {
+            text.text = handler.players[player].name + "'s Properties";
+        }
+    }
+}
